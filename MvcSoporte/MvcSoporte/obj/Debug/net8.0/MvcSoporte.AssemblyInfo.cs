a474f172b773de87fe81d1c440d0b15715b41a53
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2025-02-01 11:50:49 +0100

    migracion inicial

--- MvcSoporte/MvcSoporte/obj/Debug/net8.0/MvcSoporte.AssemblyInfo.cs
+++ MvcSoporte/MvcSoporte/obj/Debug/net8.0/MvcSoporte.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcSoporte")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b43978cae10e4d9946fd4290683e8d25c0bacd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcSoporte")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcSoporte")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
